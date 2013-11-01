60df4108988070c843cba21b80f3c541b416c27e
p-slice <p.slice169@gmail.com>
2013-10-31 20:01:46 -0700

    Helps if i update the launcher itself...

--- net/pslice/song/Launcher.java
+++ net/pslice/song/Launcher.java
@@ -4,22 +4,36 @@ import java.io.IOException;
 
 public class Launcher {
 
-    public static void main(String[] args) throws IOException {
-        Song.setKeySig("D");
+    public static int bgi = 0;
+    public static int mdi = 0;
+    public static int bsi = 0;
+
+    public static void main(String[] args){
+        Window.newWindow();
+    }
+
+    public static void beginSong(String songTitle, boolean isVerbose, String key, int bgi, int mdi) throws IOException {
+        Launcher.bgi = bgi;
+        Launcher.mdi = mdi;
+
+        Song.setKeySig(key);
+        Song.setTitle(songTitle);
         Song.setTempo();
         Song.setTimeSig();
-        Song.setVerbose(true);
+        Song.setVerbose(isVerbose);
         Song.generate();
     }
 
-    public static void returnComponents(int i, int v, int c, int b, int s, int o){
-        if (Song.isVerbose){
+    public static void returnComponents(int i, int v, int c, int b, int s, int o) {
+        if (Song.isVerbose) {
             System.out.println("Generated " + i + " intros");
             System.out.println("Generated " + v + " verses");
             System.out.println("Generated " + c + " choruses");
             System.out.println("Generated " + b + " bridges");
             System.out.println("Generated " + s + " solos");
             System.out.println("Generated " + o + " outros");
+            System.out.println();
+            System.out.println("Successfully generated new song!");
         }
     }
 }
